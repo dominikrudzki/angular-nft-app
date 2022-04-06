70eb48a56a9ce435298123ad1cc498ea7fa65ed4
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-04-07 01:32:43 +0200

    add module lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule)
+	},
+	{
+		path: 'favourites',
+		pathMatch: 'full',
+		loadChildren: () => import('./pages/favourites-page/favourites-page.module').then(m => m.FavouritesPageModule)
+	}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}

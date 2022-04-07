50995029d9de76d1e9d0f467561fd640a639e877
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-04-07 21:24:12 +0200

    add api fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { setState } from "./core/store/data/data.actions"
+import { Subscription } from "rxjs"
+import { ApiDataService } from "./core/services/api-data.service"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-nft-app';
+	dataSub$: Subscription | undefined
+
+	constructor(
+		private ApiDataService: ApiDataService,
+		private store: Store<{ data: any }>) {
+	}
+
+	ngOnInit() {
+		this.dataSub$ = this.ApiDataService.getApiData().subscribe({
+			next: ({assets}) => {
+				this.store.dispatch(setState({data: assets}))
+			}
+		})
+	}
+
+	ngOnDestroy() {
+		this.dataSub$?.unsubscribe()
+	}
 }

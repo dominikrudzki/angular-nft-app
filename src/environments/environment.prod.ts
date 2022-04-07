0898d9bb990ea30f900948837e8910bb6f0cceaf
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-04-07 21:15:58 +0200

    update env api link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
-};
+	apiUrl: 'https://api.opensea.io/api/v1/assets?format=json',
+	production: true
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+	apiUrl: 'https://api.opensea.io/api/v1/assets?format=json',
+	production: false
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
